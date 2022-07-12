4d177d91cd181a027a681f900aea24915da8b0d2
rburaksaritas <rburaksaritas@gmail.com>
2022-07-12 17:43:12 +0300

    Add feature to play a round
    Add function that plays one round of Rock, Paper, Scissors.

--- game.js
+++ game.js
@@ -6,7 +6,6 @@ function randomBetweenInterval(min = 1, max = 3){
 // Match random number with rock, paper or scissors.
 function computerPlay(){
     let randomNumber = randomBetweenInterval();
-    console.log(randomNumber);
     if (randomNumber === 1){
         return "Rock";
     } else if (randomNumber === 2){
@@ -14,3 +13,34 @@ function computerPlay(){
     } else return "Scissors";
 }
 
+// Play a single round of the game.
+// Take player selection and computer selection as parameters.
+// Return message that declares the winner.
+function round(playerSelection, computerSelection){
+    
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    console.log(playerSelection + ", " + computerSelection);
+    
+    if (playerSelection == "rock"){
+        if (computerSelection == "rock"){
+            return("Tie!");
+        } else if (computerSelection == "paper"){
+            return("You Lose! Paper beats Rock.");
+        } else return ("You Win! Rock beats Scissors.");
+    } 
+    else if (playerSelection == "paper"){
+        if (computerSelection == "rock"){
+            return("You Win! Paper beats Rock.");
+        } else if (computerSelection == "paper"){
+            return("Tie!");
+        } else return ("You Lose! Scissors beats Paper.");
+    }
+    else{
+        if (computerSelection == "rock"){
+            return("You Lose! Rock beats Scissors.");
+        } else if (computerSelection == "paper"){
+            return("You Win! Scissors beats Paper");
+        } else return ("Tie!");
+    }
+}
